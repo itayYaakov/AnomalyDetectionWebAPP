854e2e80dd0897e89c707bf30f78d3061cb34298
Itay <iyacov@gmail.com>
2021-05-20 01:18:43 +0300

    Move backend files

---
+++ backend/Controller (Server)/Server.js
@@ -0,0 +1,9 @@
+
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const host = 'localhost'
+const port = 9876
+
+
+
---
+++ backend/Model/HybridDetector.js
@@ -0,0 +1,29 @@
+
+const makeCircle = require('./MinimumCircle.js').makeCircle
+const distance = require('./MinimumCircle.js').distance
+const reg = require('./RegressionDetector')
+
+class HybridDetector extends reg.RegressionDetector{
+    constructor() {
+        super()
+        this.threshold = 0.5
+    }
+
+    isAnomaly(x, y, c) {
+        return (c.correlation >= 0.9 && super.isAnomaly(x, y, c))
+            || (c.correlation < 0.9 && c.correlation > 0.5 &&
+                (distance(x, y, c.circle.x, c.circle.y) > c.circle.r))
+    }
+
+    learnNormal(json) {
+        super.learnNormal(json);
+        for (var c of this.cf) {
+            if (c.correlation > 0.5 && c.correlation < 0.9) {
+                c.circle = makeCircle(super.makePoints(json, c.feature1, c.feature2))
+                c.circle.r *= 1.1
+            }
+        }
+    }
+}
+
+module.exports = {HybridDetector}
